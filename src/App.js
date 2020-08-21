7a85ba9af8cbc0b144a225d8eee014a86dbd5b41
Michelle Lucero <31500626+mlucero4607@users.noreply.github.com>
2020-08-21 00:00:00 -0400

    env variable added

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ import React from 'react';
 import SearchInput from './components/SearchInput';
 import WeatherInfo from './components/WeatherInfo';
 import axios from 'axios';
-//import { BrowserRouter, Route, Switch } from 'react-router-dom';
-const api = {
-  key: '',
-  base: 'https://api.openweathermap.org/data/2.5/'
+
+
+const base = 'https://api.openweathermap.org/data/2.5/';
+const key = process.env.REACT_APP_API_KEY;
   
-}
+
 
 class App extends React.Component {
+  state = { responses: [] }
 
-  state = { city: '', temp: '', description: '', iconURL: ''}
-
+  //TODO: Prop add ID for each response
   onSearchSubmit = async (entry) => {
-    const response = await axios.get(`${api.base}weather?q=${entry}&units=imperial&appid=${api.key}`)
-    this.setState({city: response.data.name, 
-                  temp: response.data.main.temp,
-                  description: response.data.weather[0].description, 
-                  iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`})
+    const response = await axios.get(`${base}weather?q=${entry}&units=imperial&appid=${key}`)
+    this.setState( {responses: this.state.responses.concat(response)} )
   }
+
 
 
   render(){
     return (
       <div className='ui container' style={{marginTop:'30px'}}>
         <SearchInput onSearchSubmit={this.onSearchSubmit}/>
-        <WeatherInfo 
-          city={this.state.city} 
-          temp={this.state.temp} 
-          description={this.state.description}
-          iconURL={this.state.iconURL}
-          />
+          {this.state.responses.map(response => (
+              <WeatherInfo 
+                // id={response.data.id}
+                city={response.data.name}
+                temp={response.data.main.temp}
+                description={response.data.weather[0].description}
+                iconURL={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+              />              
+            ))}
       </div>
     )
   }
